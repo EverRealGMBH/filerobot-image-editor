5490e2983ce6c045f147ccbe9f56912644309270
matheenshaiker <matheen.shaik@everreal.co>
2022-12-29 00:50:31 +0530

    DEV-606 image edit issue

--- packages/react-filerobot-image-editor/src/utils/imageToBase64.js
+++ packages/react-filerobot-image-editor/src/utils/imageToBase64.js
@@ -3,7 +3,10 @@ import releaseCanvas from 'utils/releaseCanvas';
 const imageToBase64 = (image) => {
   if (image instanceof HTMLImageElement) {
     const canvas = document.createElement('canvas');
-    const ctx = canvas.getContext('2d', { willReadFrequently: true });
+    const ctx = canvas.getContext('2d', {
+      willReadFrequently: true,
+      desynchronized: true,
+    });
     canvas.width = image.width;
     canvas.height = image.height;
     ctx && ctx.drawImage(image, 0, 0);
--- packages/react-filerobot-image-editor/src/utils/releaseCanvas.js
+++ packages/react-filerobot-image-editor/src/utils/releaseCanvas.js
@@ -1,7 +1,10 @@
 const releaseCanvas = (canvas) => {
   canvas.width = 1;
   canvas.height = 1;
-  const ctx = canvas.getContext('2d', { willReadFrequently: true });
+  const ctx = canvas.getContext('2d', {
+    willReadFrequently: true,
+    desynchronized: true,
+  });
   ctx && ctx.clearRect(0, 0, 1, 1);
 };
 
